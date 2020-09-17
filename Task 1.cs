bbf9433cbae6bf936f419f3f945f563ffefaa9a2
Akira <akirarizkyyy@gmail.com>
2020-09-17 23:19:43 +0300

    INIT

---
+++ Task 1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        string[] dirs;
+        string[] files;
+        public void GetInfo(string dirN)
+        {
+            Console.WriteLine("Information about Directories and Files");
+            Console.WriteLine("Directories :");
+            dirs = Directory.GetDirectories(dirN);
+            foreach(string s in dirs )
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("Files :");
+            files = Directory.GetFiles(dirN);
+            foreach (string s in files)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+        }
+    }
+    class task1
+    { 
+        static void Main(string[]args)
+        {
+            Program program = new Program();
+
+            program.GetInfo("C:\\");
+            program.GetInfo("E:\\");
+
+        }
+    }
+
+}
